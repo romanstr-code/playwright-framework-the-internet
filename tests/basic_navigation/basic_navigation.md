8539636660b18520697a04c9777e8e0e7e7c4209
romanstr-code <romanstrogoteanu1990@gmail.com>
2025-02-18 18:22:16 +0000

    Added test case to intentionally fail / Scripted to test case so it fails

--- tests/basic_navigation/basic_navigation.md
+++ tests/basic_navigation/basic_navigation.md
@@ -13,3 +13,10 @@
 2. Navigate to "https://the-internet.herokuapp.com/".
 3. Check if the page title is "Wrong Title". (This should fail)
 4. Close the browser.
+
+### Intentionally Incorrect Page - Case Fails
+
+1. Open the Browser.
+2. Navigate to " https://the-internet.herokuapp.com/ ".
+3. Check the page title with incorrect/Misspelled title "Tha InterNet" ( Test Should intentionally fail)
+4. Close the browser.
--- tests/basic_navigation/basic_navigation.spec.js
+++ tests/basic_navigation/basic_navigation.spec.js
@@ -12,4 +12,22 @@ test.describe('Basic Navigation', () => {
         // Assert that the page title is exactly "The Internet"
         await expect(page).toHaveTitle('The Internet');
     });
+
+    // Negative Test Case: Ensure that the page title is incorrect
+    test('Negative: Check Incorrect page title', async ({ page}) => {
+        //Navigate to the target website
+        await page.goto('https://the-internet.herokuapp.com/');
+
+        //Assert that the page title is not "Wrong Title"
+        await expect(page).not.toHaveTitle('Worng Title'); // Using NOT for negative assertion
+    });
+
+    // Intentionally failing test case to demonstrate the negative assertion
+    test('negative: Intentionally failing test', async ({ page }) => {
+        //Navigate to the target website
+        await page.goto('https://the-internet.herokuapp.com/');
+
+        //Assert that the page title is intentionally incorrect "Tha InterNet"
+        await expect(page).toHaveTitle('Tha InterNet'); // This intentionally fails
+    });
 });
