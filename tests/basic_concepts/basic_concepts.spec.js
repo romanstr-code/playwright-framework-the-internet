18168718493d30f832182fe48c7a92933d3a29dd
romanstr-code <69579481+romanstr-code@users.noreply.github.com>
2025-02-19 14:08:00 +0000

    Basic concepts with playwright | Browser, Context, Page Example |

---
+++ tests/basic_concepts/basic_concepts.spec.js
@@ -0,0 +1,20 @@
+const { test } = require('@playwright/test');
+
+test('Browser, Context, Page example', async ({ browser }) => {
+    // 'browser' fixture is available
+    console.log('Browser type:', browser.browserType().name()); // Get the browser type (chromium, firefox, webkit)
+
+    const context = await browser.newContext(); // Create a new browser context
+    const page1 = await context.newPage(); // Create a new page in the context
+
+    await page1.goto('https://example.com');
+    console.log('Page 1 Title:', await page1.title());
+
+    const page2 = await context.newPage(); // Create another page in the same context
+
+    await page2.goto('https://wikipedia.org');
+    console.log('Page 2 title:', await page2.title());
+
+    await context.close(); // Close the browser context (and all pages within it)
+    // Browser is automatically closed by Playwright after all tests are done in this file.
+});
